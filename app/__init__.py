c892ba6716e387523eea9c8e16fb164e0787e6e0
huy6879 <117825088+huy6879@users.noreply.github.com>
2023-12-21 12:31:04 +0700

    abcde

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 from urllib.parse import quote
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-#them command cho vui
+
 app = Flask(__name__)
 
 app.secret_key = 'DAFDJFSKDFNSKDF*&^yFDSNFBDSBFDSBFSFJDSNFJIDFJS'
-app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/flydbv1?charset=utf8mb4" % quote('PHW#84#jeor')
+app.config["SQLALCHEMY_DATABASE_URI"] ="mysql+pymysql://root:%s@localhost/flydbv1?charset=utf8mb4" % quote('Admin@123')
 
 db = SQLAlchemy(app=app)
 login = LoginManager(app=app)
